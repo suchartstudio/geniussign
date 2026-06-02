81d4e951e3d935083bb6f263740df22c404f4560
suchartstudio <suchartstudio@gmail.com>
2026-06-02 23:43:08 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, onValue, set, update } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0kMrUtVNSIzlAF9Rm6pSlJQ5eRZ3pk90",
+  authDomain: "signage-b807a.firebaseapp.com",
+  databaseURL: "https://signage-b807a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "signage-b807a",
+  storageBucket: "signage-b807a.firebasestorage.app",
+  messagingSenderId: "777605695730",
+  appId: "1:777605695730:web:bc0760e01e4022d94be86b",
+  measurementId: "G-VW6B5QM551"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, onValue, set, update };
